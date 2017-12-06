f6ce75c79441c972aa9f829e9ffd89afa3eddd28
Dustin K <miduku11@gmail.com>
2017-12-06 21:59:01 +0100

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# retro-future-hauptaufgabe
+
+> Project made at the University of Applied Sciences Potsdam; Course 21FVId-XD Retro-Future-Webdesign; WiSe 2017/18
+
+## Build Setup
+
+``` bash
+# Use Node 8.9.0
+
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'retro-future-hauptaufgabe',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Project made at the University of Applied Sciences Potsdam; Course 21FVId-XD Retro-Future-Webdesign; WiSe 2017/18' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
