355dd014c02418761b48f75f1abca82d20d6bffa
Dustin K <miduku11@gmail.com>
2018-01-24 00:22:23 +0100

    update calendar

--- layouts/calendar.vue
+++ layouts/calendar.vue
@@ -12,8 +12,11 @@ export default {
 @import '../assets/css/_vars';
 @import '../assets/css/_mixins';
 
+
 #tpl-calendar {
   &.container {
+    $fontSizeWidescreen: 2.25vw;
+
     .cal {
       font-family: $fontSans;
       width: 100%;
@@ -23,8 +26,12 @@ export default {
       text-align: center;
       margin: 1em 0;
 
+      @include widescreen {
+        // font-size: 1.85rem;
+      }
+
       .row {
-        background: pink;
+        // background: pink;
         width: 100%;
         margin: 0;
         line-height: 1.25;
@@ -33,12 +40,42 @@ export default {
       .year {
         .year-name {
           font-weight: normal;
-          font-size: 2.5vw;
+          font-size: 1em;
           display: inline-block;
+
+          @include widescreen {
+            font-size: $fontSizeWidescreen;
+          }
         }
       }
 
       .month {
+        &.is-active {
+          .month-name {
+            opacity: 1;
+          }
+
+          ul.days {
+            font-size: 1em;
+            height: unset;
+            transition: $transition;
+
+            li.day .day-number {
+              // font-size: 2.15vw;
+              font-size: 1em;
+              transition: $transition;
+
+              @include widescreen {
+                font-size: $fontSizeWidescreen;
+              }
+            }
+
+              ul.events {
+                display: block !important;
+              }
+          }
+        }
+
         ul {
           margin: 0;
           padding: 0;
@@ -51,88 +88,222 @@ export default {
 
         .month-name {
           font-weight: normal;
-          font-size: 2.5vw;
+          font-size: 1em;
+          // font-size: 2.5vw;
           display: inline-block;
+          cursor: pointer;
+          opacity: .25;
+
+          &:focus,
+          &:hover {
+            opacity: .5;
+          }
+
+          @include widescreen {
+            font-size: $fontSizeWidescreen;
+          }
         }
 
-        ul.month-days {
+        ul.days {
           margin: -.25em .5em 0;
-          background: blueviolet;
+          // background: blueviolet;
           display: block;
+          font-size: 0;
+          height: 0;
 
-          > li {
+          .day-overlay {
+            background: #fff;
+            opacity: .9;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            cursor: pointer;
+          }
+
+          li.day {
             display: inline-block;
             position: relative;
-            opacity: .75;
+            height: 1em;
+            width: 1.25em;
 
-            &.has-entry {
-              opacity: 1;
+            @include widescreen {
+              width: $fontSizeWidescreen * 1.25;
             }
 
-            > span {
-              cursor: pointer;
-              display: inline-block;
-              font-size: 2.15vw;
-            }
+            &.is-active {
+              .day-container {
+                position: absolute;
+                z-index: 1;
+                left: .1em;
+                top: -.1em;
+                width: 100%;
+                height: 100%;
+                transition: $transition;
 
-            ul.month-events {
-              position: absolute;
-              text-align: left;
-              background: purple;
-              width: 35vw;
-              left: (-35vw/2);
-              font-size: 0;
-
-              @include widescreen {
-                width: 400px;
-                left: (-400px/2);
-              }
-
-              &.is-active {
-                font-size: 1em;
-
-                h3 {
-                  transform: translateY(1.25em);
-                }
-
-                .time {
-                  transform: translateY(0)
-                }
-              }
-
-
-              li.month-event {
-                position: relative;
-                h3 {
-                  font-size: 1.75em;
-                  margin-bottom: 1.5em;
+                .day-number {
+                  transform: scale(1.25);
                   transition: $transition;
+                }
+
+                .events {
+                  .event {
+                    .img {
+                      width: 25%;
+                      transition: $transition;
+                    }
+
+                    &.is-active {
+                      .img {
+                        top: 0;
+                        width: 40%;
+                      }
+                    }
+                  }
+                }
+              }
+
+              ul.events {
+                font-size: 1em;
+                transition: $transition;
+              }
+            }
+
+            .day-container {
+              opacity: .5;
+
+              &.has-events {
+                opacity: 1;
+
+                .day-number {
                   cursor: pointer;
                 }
+              }
+            }
 
-                .time {
-                  color: #DC0215;
-                  transform: rotateX(180deg) translateY(-.75em);
-                  font-size: 2.5em;
-                  position: absolute;
-                  top: 0;
-                  transition: $transition;
+              .day-number {
+                display: inline-block;
+                font-size: 0;
+              }
+
+              ul.events {
+                position: absolute;
+                text-align: left;
+                // background: purple;
+                width: 35vw;
+                left: calc(-35vw/2 + .5em);
+                font-size: 0;
+                display: none;
+
+                @include widescreen {
+                  width: 400px;
+                  left: (-400px/2);
                 }
 
-                .desc {
-                  font-size: 0;
-                  transition: $transition;
+                &.is-active {
+                }
 
-                  &.is-active {
-                    font-size: 1em;
+                li.event {
+                  position: relative;
+                  // background: greenyellow;
+
+                  + li.event {
+                    margin-top: 2em;
                   }
 
-                  p {
-                    font-family: $fontSerif;
+                  &.is-active {
+                    h3 {
+                      transition: $transition;
+                      transform: translateY(1.25em);
+                      margin-bottom: 1.5em;
+                    }
+
+                    .time {
+                      transition: $transition;
+                      transform: translateY(0)
+                    }
+
+                    .img {
+                      padding-top: 15%;
+                      width: 50%;
+                      right: 0;
+                    }
+                  }
+
+                  .event-click-container {
+                    cursor: pointer;
+                  }
+
+                  h3 {
+                    font-size: 1.75em;
+                    margin-bottom: 1em;
+                    line-height: 1;
+                    position: relative;
+                    z-index: 1;
+                    // transition: $transition;
+                  }
+
+                  .time {
+                    color: #DC0215;
+                    transform: rotateX(180deg) translateY(-.35em);
+                    font-size: 2.5em;
+                    position: absolute;
+                    top: 0;
+                  }
+
+                  .desc {
+                    opacity: 0;
+                    line-height: 0;
+
+                    &.is-active {
+                      transition: $transition;
+                      opacity: 1;
+                      line-height: 1.25;
+
+                      p {
+                        margin-bottom: .5em;
+                        transition: $transition;
+                      }
+                    }
+
+                    p {
+                      font-family: $fontSerif;
+                      margin-bottom: 0;
+                    }
+                  }
+
+                  .img {
+                    position: absolute;
+                    top: 5%;
+                    right: 25%;
+                    padding-top: 0%;
+                    width: 0;
+                    height: 0;
+                    z-index: -1;
+                    transform: rotate(-15deg);
+                  }
+
+                  .venue {
+                    // width: 100%;
+                    color: #DC0215;
+                    text-align: center;
+                    opacity: 0;
+
+                    &.is-active {
+                      transition: $transition;
+                      transform: translateY(.5em);
+                      opacity: 1;
+                    }
+
+                    span {
+                      transform: rotate(175deg);
+                      display: inline-block;
+                    }
                   }
                 }
               }
-            }
           }
         }
       }
--- pages/termine.vue
+++ pages/termine.vue
@@ -9,7 +9,7 @@
       <div class="rootColumn is-center">
         <div class="cal">
           <a
-            @click.prevent="createYear(calendar.name - 1)"
+            @click.prevent="changeYear(calendar.name - 1)"
             href="#"
             class="row year"
           >
@@ -19,48 +19,94 @@
           <div
             v-for="month in calendar.months"
             :key="month"
+            :class="{ 'is-active': month.isActive }"
             class="row month"
           >
-            <h2 class="month-name">
+            <h2
+              @click="toggleItems(month, calendar.months)"
+              class="month-name"
+            >
               {{ month.name }}
             </h2>
 
-            <ul class="month-days">
+            <ul
+              class="days"
+            >
               <li
                 v-for="day in month.days"
                 :key="day"
+                :class="{ 'is-active': day.isActive }"
+                class="day"
               >
-                <span @click="day[1].isActive = !day[1].isActive">
-                  {{ day[0] }}.
-                </span>
 
-                <ul
-                  v-if="day[1].eventList.length > 0"
-                  :class="{ 'is-active': day[1].isActive }"
-                  class="month-events"
+                <div
+                  @click="closeOverlay(day)"
+                  v-show="day.isOverlay"
+                  class="day-overlay"
+                ></div>
+
+                <div
+                  v-if="day.events.length > 0"
+                  class="day-container has-events"
                 >
-                  <li
-                    v-for="event in day[1].eventList"
-                    :key="event"
-                    class="month-event"
+                  <span
+                    @click="toggleItems(day, month.days, true)"
+                    class="day-number"
                   >
-                    <h3 @click="event.isActive = !event.isActive">{{ event.title }}</h3>
-                    <span class="time">{{ event.time[0] }} – {{ event.time[1] }}</span>
+                    {{ day.number }}.
+                  </span>
 
-                    <div
-                      v-html="event.desc"
-                      :class="{ 'is-active': event.isActive }"
-                      class="desc"
+                  <ul class="events">
+                    <li
+                      v-for="event in day.events"
+                      :key="event"
+                      :class="{ 'is-active': event.isActive && day.isActive }"
+                      class="event"
                     >
-                    </div>
-                  </li>
-                </ul>
+                      <div class="img">
+                        <img :src="event.img" alt="">
+                      </div>
+
+                      <div
+                        @click="toggleItems(event, day.events)"
+                        class="event-click-container"
+                      >
+                        <h3>{{ event.title }}</h3>
+                        <span class="time">{{ event.time[0] }} – {{ event.time[1] }}</span>
+                      </div>
+
+                      <div
+                        v-html="event.desc"
+                        :class="{ 'is-active': event.isActive && day.isActive}"
+                        class="desc"
+                      >
+                      </div>
+
+                      <div
+                        :class="{ 'is-active': event.isActive && day.isActive}"
+                        class="venue"
+                      >
+                        <span>{{ event.venue }}</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+
+                <div
+                  v-else
+                  class="day-container"
+                >
+                  <span class="day-number">
+                    {{ day.number }}.
+                  </span>
+                </div>
+
               </li>
             </ul>
           </div>
 
           <a
-            @click.prevent="createYear(calendar.name + 1)"
+            @click.prevent="changeYear(calendar.name + 1)"
             href="#"
             class="row year"
           >
@@ -87,9 +133,7 @@ export default {
 
   data () {
     return {
-      calendar: {},
-      isActive: false,
-      isActiveChild: false
+      calendar: {}
     }
   },
 
@@ -97,62 +141,111 @@ export default {
     // DOM ready
     this.$nextTick(() => {
       // init
-      // this.createYear(2018)
-      console.log(this.createYear(2018))
+      this.createYear(new Date())
+      // console.log(this.createYear(2018))
     })
   },
 
   methods: {
-    createYear (year) {
-      const d = new Date(year, 0, 1)
+    closeOverlay (itm) {
+      itm.isActive = false
+      itm.isOverlay = false
+    },
 
+    toggleItems (item, items, hasOverlay = false) {
+      for (const itm of items) {
+        if (itm === item) {
+          itm.isActive = true
+          if (hasOverlay) {
+            itm.isOverlay = true
+          }
+        } else {
+          itm.isActive = false
+          if (hasOverlay) {
+            itm.isOverlay = false
+          }
+        }
+      }
+    },
+
+    changeYear (newYear) {
+      const d = new Date()
+      d.setFullYear(newYear)
+
+      this.createYear(d)
+    },
+
+    createYear (date) {
+      const now = {
+        d: date,
+        year: date.getFullYear(),
+        month: date.getMonth()
+      }
+
+      const d = new Date(now.year, 0, 1)
+
+      let monthCounter = 0
       let calendar = {}
       let calYear = calendar = {
-        name: year,
+        name: now.year,
         months: []
       }
-      let month = 0
 
       // do while on correct year
-      while (year === d.getFullYear()) {
-        const monthName = d.toLocaleString('de-de', { month: 'long' }) // get month name
-        // let eventContent = null
+      while (now.year === d.getFullYear()) {
+        // get month name
+        const monthName = d.toLocaleString('de-de', { month: 'long' })
+
         let calMonth = calYear.months[d.getMonth()] = {
           name: monthName,
+          isActive: (d.getMonth() === now.month && d.getFullYear() === now.year),
           days: []
         }
 
-        while (month === d.getMonth()) {
+        // do while on correct month
+        while (monthCounter === d.getMonth()) {
           // compare date with events date
           // then add event to calendar
-          let eventContent
+          let eventList = []
 
           if (events.length > 0) {
-            eventContent = {
-              isActive: false,
-              eventList: []
-            }
-
             for (let i = 0; i < events.length; i++) {
               const event = events[i]
-
               const eventDateFrom = new Date(event.date + event.time[0])
               const eventDateTo = new Date(event.date + event.time[1])
 
-              if (`${eventDateFrom.getFullYear()}-${eventDateFrom.getMonth()}-${eventDateFrom.getDate()}` === `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`) {
+              const eventDate = {
+                from: {
+                  year: eventDateFrom.getFullYear(),
+                  month: eventDateFrom.getMonth(),
+                  day: eventDateFrom.getDate(),
+                  hour: eventDateFrom.getHours(),
+                  minute: eventDateFrom.getMinutes()
+                },
+                to: {
+                  year: eventDateTo.getFullYear(),
+                  month: eventDateTo.getMonth(),
+                  day: eventDateTo.getDate(),
+                  hour: eventDateTo.getHours(),
+                  minute: eventDateTo.getMinutes()
+                }
+              }
+
+              // event times
+              if (`${eventDate.from.year}-${eventDate.from.month}-${eventDate.from.day}` === `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`) {
                 const t = {
                   from: {
-                    h: ('0' + eventDateFrom.getHours()).slice(-2),
-                    m: ('0' + eventDateFrom.getMinutes()).slice(-2)
+                    h: ('0' + eventDate.from.hour).slice(-2),
+                    m: ('0' + eventDate.from.minute).slice(-2)
                   },
                   to: {
-                    h: ('0' + eventDateTo.getHours()).slice(-2),
-                    m: ('0' + eventDateTo.getMinutes()).slice(-2)
+                    h: ('0' + eventDate.to.hour).slice(-2),
+                    m: ('0' + eventDate.to.minute).slice(-2)
                   }
                 }
 
-                // push dat s**t
-                eventContent.eventList.push({
+                // push dat event
+                eventList.push({
                   title: event.title,
                   time: [
                     `${t.from.h}:${t.from.m}`,
@@ -160,21 +253,27 @@ export default {
                   ],
                   desc: event.desc,
                   img: event.img,
+                  venue: event.venue,
                   isActive: false
                 })
               }
             }
           }
 
-          calMonth.days.push([d.getDate(), eventContent])
-          // console.log(calMonth.days)
+          calMonth.days.push({
+            number: d.getDate(),
+            events: eventList,
+            isActive: false,
+            isOverlay: false
+          })
+
+          // count day +1
           d.setDate(d.getDate() + 1)
-          // eventContent = null
         }
 
         // reset days and add to month counter
         d.setDate(1)
-        month = month + 1
+        monthCounter = monthCounter + 1
       }
 
       this.calendar = calendar
